2006e01975a015c6b0bbbcc9e12644f379af9f85
Юра Олексюк <yuriy.olexiuk@gmail.com>
2018-08-08 15:05:02 +0300

    Secon commit

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 jQuery('.panel-heading').click(function() {
     jQuery(this).siblings('.panel-body').slideToggle();
+    jQuery(this).children('.toggleIcons').children().siblings().toggle();
 });
 
-dragDrop('.moveItem', 'leftPanel', 'rightPanel');
+dragDrop('.moveItem', '', '');
 
 function dragDrop(item, firstParent, secondParent) {
     jQuery(item).click(function() {
-
         if (jQuery(this).parents('#selectedList').length) {
             var parent = '#' + jQuery(this).parents('.group-item').attr('data-parent');
             jQuery(parent).append(jQuery(this).parents('.group-item'));
@@ -15,25 +15,6 @@ function dragDrop(item, firstParent, secondParent) {
         }
         jQuery('#summBox').html(reculcPrice());
         jQuery(this).children().siblings().toggle();
-
-        // console.log(jQuery(this).parent('#selectedList') );
-
-        // var id = jQuery(this).parents('.list-group')[0].id;
-        // console.log(id);
-        // var replaced;
-        // if(id.indexOf('left') + 1) {
-        //     replaced = id.replace(/left/gi, '#right');
-        // } else {
-        //     replaced = id.replace(/right/gi, '#left');
-        // }
-        //
-        // jQuery(this).parent('.group-item').hide();
-        // jQuery('#selectedList').append(jQuery(this).parents('.group-item'));
-        // jQuery(this).parent('.group-item').show('slow');
-        // console.log(jQuery(this).parents('.group-item'));
-        //
-        //
-        //
     });
 }
 
