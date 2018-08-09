cbc00f47293f0669b1d7558fded08880bcb18323
Юра Олексюк <yuriy.olexiuk@gmail.com>
2018-08-09 09:49:01 +0300

    Hello

--- js/index.js
+++ js/index.js
@@ -6,17 +6,49 @@ jQuery('.panel_header').click(function() {
 dragDrop('.moveItem', '', '');
 
 function dragDrop(item, firstParent, secondParent) {
+    var arr = JSON.parse(jQuery.cookie('items'));
+
+
+    console.log(arr);
+    // savedItems.each(function() {
+    //
+    // });
+    for (var i = 0; i < arr.length; i++) {
+        jQuery('#selectedList').append(jQuery('#' + arr[i]));
+    }
+    jQuery('#summBox').html(reculcPrice());
+    // jQuery(this).children().siblings().toggle();
+
     jQuery(item).click(function() {
         if (jQuery(this).parents('#selectedList').length) {
             var parent = '#' + jQuery(this).parents('.group-item').attr('data-parent');
             jQuery(parent).append(jQuery(this).parents('.group-item'));
+
+            var indexOfItem = arr.indexOf(jQuery(this).parents('.group-item').attr('id'));
+            arr.splice(indexOfItem);
+            console.log(arr);
         } else {
             jQuery('#selectedList').append(jQuery(this).parents('.group-item'));
+
+            arr.push(jQuery(this).parents('.group-item').attr('id'));
+            console.log(arr);
         }
-        jQuery('#summBox').html(reculcPrice());
+        jQuery.cookie('items', JSON.stringify(arr));
+
+
+
+
         jQuery(this).children().siblings().toggle();
+        jQuery('#summBox').html(reculcPrice());
+
+
+    });
+
+    jQuery('#saveItems').click(function() {
+
     });
 }
+
 
 function reculcPrice() {
     var price = 0;
